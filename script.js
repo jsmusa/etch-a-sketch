fdabe4ab89b507d786ec102b6370e94405482765
Jojo <jonathansardeamusa@gmail.com>
2022-06-22 15:57:47 +0800

    added hover effects

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const gridContainer = document.getElementById('grid-container');
 const div = [];
+let gridSize = 16;
 
-console.log(gridContainer)
-
-for (i=0; i<16*16; i++) {
+for (i=0; i<gridSize*gridSize; i++) {
     div[i] = document.createElement('div');
-    div[i].className = 'grid-box';
-    div[i].style.width = `${640/16}px`;
+    div[i].classList.add('grid-box');
+    div[i].style.width = `${400/gridSize}px`;
     gridContainer.appendChild(div[i]);
 }
 
+div[0].classList.add = 'hover';
+
+for(const single of div) {
+    single.addEventListener('mouseenter',(e) => {
+        e.target.classList.add('hover');
+    })
+}
