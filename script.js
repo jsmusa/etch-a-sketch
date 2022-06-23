fb71f41306829287f3ea42091f5c20eea027d8f1
Jojo <jonathansardeamusa@gmail.com>
2022-06-23 15:03:28 +0800

    added feature to change gridSize

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ const div = [];
 const slider = document.querySelector('#slider');
 let gridSize = 16;
 
+createGrid();
+
 slider.addEventListener('input',() => {
+    for (let i=0; i<div.length; i++) {
+        div[i].remove();
+    }
     gridSize = slider.value;
-    console.log(gridSize);
+    createGrid();
 })
 
 // creates grid elements
-for (i=0; i<gridSize*gridSize; i++) {
-    div[i] = document.createElement('div');
-    div[i].classList.add('grid-box');
-    div[i].style.width = `${400/gridSize}px`;
-    gridContainer.appendChild(div[i]);
-}
+function createGrid() {
+    for (let i=0; i<gridSize*gridSize; i++) {
+        div[i] = document.createElement('div');
+        div[i].classList.add('grid-box');
+        div[i].style.width = `${400/gridSize}px`;
+        gridContainer.appendChild(div[i]);
+    }
 
-//gives hover effect on grid elements 
-for(const single of div) {
+    //gives hover effect on grid elements 
+    for(const single of div) {
     single.addEventListener('mouseenter',addHover);
+    }
 }
 
 function addHover(e) {
